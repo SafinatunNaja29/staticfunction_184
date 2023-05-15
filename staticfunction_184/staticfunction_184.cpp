93b737497713581436e4d22cbf3794b55fa09362
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-05-15 15:38:12 +0700

    prosedure mahasiswa

--- staticfunction_184/staticfunction_184.cpp
+++ staticfunction_184/staticfunction_184.cpp
@@ -11,5 +11,16 @@ public :
 	void setID();
 	void printAll();
 	Mahasiswa(string pnama) :nama(pnama) { setID(); }
-
 };
+
+int Mahasiswa::nim = 0;
+
+void Mahasiswa::setID() {
+	id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
