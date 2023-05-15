fc9634a41993c3c1c3a4b33902d44427149ad81b
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-05-15 15:33:55 +0700

    object Mahasiswa

---
+++ staticfunction_184/staticfunction_184.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa {
+public :
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+	Mahasiswa(string pnama) :nama(pnama) { setID(); }
+
+};
