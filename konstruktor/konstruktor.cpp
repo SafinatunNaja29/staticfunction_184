571f4c49a447c7124bfc14a18e8390166e46a1d8
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-05-15 17:49:00 +0700

    program utama konstruktor

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -36,3 +36,16 @@ void Mahasiswa::cetak() {
 	cout << "Nama =" << nama << endl;
 }
 
+int main() {
+	Mahasiswa mhs1;
+	Mahasiswa mhs2(20);
+	Mahasiswa mhs3("Indra");
+	Mahasiswa mhs4("30, Fauzan");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+}
